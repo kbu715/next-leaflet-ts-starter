000711bb76e94faaf3c4252d3b4885b9eba1c453
kbu715 <kbu715@gmail.com>
2022-09-25 20:10:27 +0900

    🍱 Add Icons

---
+++ vendor/index.ts
@@ -0,0 +1,14 @@
+import { icon } from "leaflet";
+const CarIcon = icon({
+  iconUrl: "Car.svg",
+  iconSize: [24, 24],
+  iconAnchor: [12, 24],
+});
+
+const ArrowIcon = icon({
+  iconUrl: "Car.svg",
+  iconSize: [24, 24],
+  iconAnchor: [12, 24],
+});
+
+export { CarIcon, ArrowIcon };
